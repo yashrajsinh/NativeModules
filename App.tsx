1ca19369f2cd24f9494c6ffc689604e84bcb62e4
yash <yashrajsinhjadeja2253@gmail.com>
2026-03-27 16:53:09 +0530

    works fine

--- App.tsx
+++ App.tsx
@@ -5,19 +5,51 @@
  * @format
  */
 
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-
+//custom toast
+import Toast from 'react-native-toast-message';
 function App() {
+  const handleAndroid = () => {
+    Toast.show({
+      type: 'info',
+      position: 'bottom',
+      text1: 'Showing Android device info 🤖',
+    });
+  };
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={styles.container}></SafeAreaView>
+      <SafeAreaView style={styles.container}>
+        <TouchableOpacity
+          style={styles.button}
+          activeOpacity={0.4}
+          onPress={() => handleAndroid()}
+        >
+          <Text style={styles.text}> Show Android Device Details 🤖 </Text>
+        </TouchableOpacity>
+        <Toast />
+      </SafeAreaView>
     </SafeAreaProvider>
   );
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  button: {
+    backgroundColor: '#4CAF50', // nice green
+    paddingVertical: 14,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 5, // Android shadow
+    marginHorizontal: 20, // adds space left & right
+  },
+  text: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: '600',
   },
 });
 
