78cbbfe494441c5c3c217aa02114b25012db2074
yash <yashrajsinhjadeja2253@gmail.com>
2026-03-30 16:12:58 +0530

    android both button works
    - shows an image from a  native module

---
+++ android/app/src/main/java/com/nativemodules/imageviewer/ImageActivity.java
@@ -0,0 +1,26 @@
+package com.nativemodules.imageviewer;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.nativemodules.R;
+
+public class ImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_image);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
